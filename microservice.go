4376c46276233f932ef75d7a039b797e8d1551dc
mohammad ahmadi <myprojectsfile@hotmail.com>
2018-02-23 00:19:31 +0330

    Initial Commit

---
+++ microservice.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	// "github.com/PacktPublishing/Cloud-Native-Go/api"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+
+	// http.HandleFunc("/api/books/", api.BookHandleFunc)
+
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+	port := os.Getenv("CNATIVE_PORT")
+	if len(port) == 0 {
+		port = "9090"
+	}
+	return ":" + port
+}
+
+func index(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusOK)
+	fmt.Fprintf(res, "Welcome to Cloud Native Go (Update).")
+}
